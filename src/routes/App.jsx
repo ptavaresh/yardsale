cbea342c01950747dcb24842068a7357612fb6ab
Pedro Tavares <ptavaresh@gmail.com>
2022-03-31 11:38:06 -0600

    class 22 context shopping cart

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '../containers/Layout';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
+import Layout from '@containers/Layout';
+import Home from '@pages/Home';
+import Login from '@pages/Login';
 import RecoveryPassword from '../pages/RecoveryPassword';
-import SendEmail from '../pages/SendEmail';
-import NewPassword from '../pages/NewPassword';
-import MyAccount from '../pages/MyAccount';
-import CreateAccount from '../pages/CreateAccount';
-import Checkout from '../pages/Checkout';
-import Orders from '../pages/Orders';
-import NotFound from '../pages/NotFound';
+import SendEmail from '@pages/SendEmail';
+import NewPassword from '@pages/NewPassword';
+import MyAccount from '@pages/MyAccount';
+import CreateAccount from '@pages/CreateAccount';
+import Checkout from '@pages/Checkout';
+import Orders from '@pages/Orders';
+import NotFound from '@pages/NotFound';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 
 import '../styles/global.scss';
 const App = () => {
+	const initialState = useInitialState();
 	return (
+		<AppContext.Provider value={initialState}>
 		<BrowserRouter>
 		<Layout>
 		  <Routes>
@@ -32,6 +36,7 @@ const App = () => {
 		  </Routes>
 		</Layout>
 	  </BrowserRouter> 
+	  </AppContext.Provider>
 		
 	);
 }
